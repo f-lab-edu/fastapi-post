b4f077d6e7345ab03f31176d1637f139a46042d2
ksg9482 <ksg930523@gmail.com>
2024-09-19 18:53:49 +0900

    유저 프로필 이미지 저장 추가

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings
 class Config(BaseSettings):
     DATABASE_URL: str = Field(default="sqlite+aiosqlite:///:memory:")
     REDIS_URL: str = Field(default="redis://localhost")
+    UPLOAD_DIR: str = Field(default="./profile_img")
 
 
 config = Config()
